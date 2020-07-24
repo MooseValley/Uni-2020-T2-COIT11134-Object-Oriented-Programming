1598b030d9cf08be56c2e854a4e1b0534d025ff6
MooseValley <Moose_Software@yahoo.com.au>
2020-07-24 22:48:27 +1000

    Updates

---
+++ 00__create_Week_01_12_folders.bat
@@ -0,0 +1,16 @@
+echo off
+cls
+mkdir "Week 01"
+mkdir "Week 02"
+mkdir "Week 03"
+mkdir "Week 04"
+mkdir "Week 05"
+mkdir "Week 06"
+mkdir "Week 07"
+mkdir "Week 08"
+mkdir "Week 09"
+mkdir "Week 10"
+mkdir "Week 11"
+mkdir "Week 12"
+echo DONE !
+pause
